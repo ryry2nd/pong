9130dde450895fa0ee326dbf75d5e91f894b92c9
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-03-05 20:21:25 -0500

    the ball has volocity, also it is a square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #imports
-from cv2 import circle
-import pygame, os
+import pygame, os, math, random
 
 #inits
 pygame.init()
@@ -20,35 +19,63 @@ BLACK = (0, 0, 0)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
-# paddle
+# paddle object
 class Paddle:
-    def __init__(self, PWIDTH, PHEIGHT, POS):#initualise the vars
-        self.WIDTH = PWIDTH
-        self.HEIGHT = PHEIGHT
+    def __init__(self, size, POS):#initualise the vars
+        self.WIDTH = size[0]
+        self.HEIGHT = size[1]
         self.x = POS[0]
         self.y = POS[1]
     def make_it(self):# makes the paddle
         PADDLE = pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT)
         pygame.draw.rect(WIN, WHITE, PADDLE)
 
-#ball
+#ball object
 class Ball:
-    def __init__(self, POS, size):
+    #def vars
+    xVel = 0
+    yVel = 0
+
+    def __init__(self, size, POS):# init vars
+        self.WIDTH = size[0]
+        self.HEIGHT = size[1]
         self.x = POS[0]
         self.y = POS[1]
-        self.size = size
-    def make_it(self):
-        pygame.draw.circle(WIN, WHITE, (self.x, self.y), self.size)
-        
+    def make_it(self):#makes the paddle
+        BALL = pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT)
+        pygame.draw.rect(WIN, WHITE, BALL)
+    def move(self, players):# moves the player
+        BALL = pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT)
 
+        # bounces off the walls
+        if self.x + self.xVel <= 0 or self.x + self.xVel >= WIDTH - self.WIDTH:
+            self.xVel = -self.xVel
+        if self.y + self.yVel <= 0 or self.y + self.yVel >= HEIGHT - self.HEIGHT:
+            self.yVel = -self.yVel
+        
+        for player in players:# loops through the paddles
+            if BALL.colliderect(pygame.Rect(player.x, player.y, player.WIDTH, player.HEIGHT)):# runs when there is a colision
+                self.xVel = -self.xVel# reverses the balls x vel
+                self.yVel = -((((player.y + (player.HEIGHT // 2)) - self.y) - 10) // 10)# sets the y level based on where it hits the paddle
+                if abs(self.xVel) < 30:# runs when it is below the max sppeed
+                    # make the ball faster
+                    if self.xVel > 0:
+                        self.xVel += 1
+                    else:
+                        self.xVel -= 1
+
+        #move the ball
+        self.x += self.xVel
+        self.y += self.yVel
+        
 #main funtion
 def main():
     #init objects
-    PLAYER1 = Paddle(10, 100, (30, HEIGHT // 2 - 50))
-    PLAYER2 = Paddle(10, 100, (WIDTH - 40, HEIGHT // 2 - 50))
-    BALL = Ball((WIDTH//2,HEIGHT//2), 10)
-    
-    BALL_VEL = 0
+    PLAYER1 = Paddle((20, 100), (60, HEIGHT // 2 - 50))
+    PLAYER2 = Paddle((20, 100), (WIDTH - 70, HEIGHT // 2 - 50))
+    BALL = Ball((20, 20), (WIDTH//2 - 10, HEIGHT//2 - 10))
+
+    BALL.xVel = -1# default vel
 
     run = True
     clock = pygame.time.Clock()#defines the clock
@@ -77,11 +104,13 @@ def main():
 
         WIN.fill(BLACK)# fills the screen
 
+        BALL.move((PLAYER1, PLAYER2)) #move the ball
+
         #makes the objects
         PLAYER1.make_it()
         PLAYER2.make_it()
         BALL.make_it()
-        
+
         pygame.display.update()# updates the display
         
     pygame.quit()#quits
