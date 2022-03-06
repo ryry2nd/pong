644f965f16f248f70268b0c64d0adad32e14ac05
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-03-05 21:34:47 -0500

    there is now a scoring system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #imports
-import pygame, os, math, random
+import pygame, os
 
 #inits
 pygame.init()
@@ -11,6 +11,10 @@ WIDTH, HEIGHT = 900, 500
 FPS = 60
 VEL = 5
 
+#fonts
+SCORE_FONT = pygame.font.SysFont('comicsans', 100)
+WIN_FONT = pygame.font.SysFont('comicsans', 100)
+
 #colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -18,6 +22,9 @@ BLACK = (0, 0, 0)
 #screen
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
+
+# sets the points
+p1Points, p2Points = 0, 0
 
 # paddle object
 class Paddle:
@@ -48,8 +55,6 @@ class Ball:
         BALL = pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT)
 
         # bounces off the walls
-        if self.x + self.xVel <= 0 or self.x + self.xVel >= WIDTH - self.WIDTH:
-            self.xVel = -self.xVel
         if self.y + self.yVel <= 0 or self.y + self.yVel >= HEIGHT - self.HEIGHT:
             self.yVel = -self.yVel
         
@@ -67,17 +72,39 @@ class Ball:
         #move the ball
         self.x += self.xVel
         self.y += self.yVel
+
+# checks if there is a win
+def checkWin():
+    win = None
+
+    if p1Points == 7:# if there is a win, set a winner
+        win = 'Player1'
+    elif p2Points == 7:
+        win = 'Player2'
+    
+    if win:# if there is a win print it out and update the screen
+        WIN.fill(BLACK)
+        WIN.blit(WIN_FONT.render(win + " wins!", 1, WHITE), (0 + 100, HEIGHT//2 - 50))
+        pygame.display.update()
+        pygame.time.delay(5000)
+    
+    return win #returns the winner
         
+
 #main funtion
 def main():
+    #init vars
+    global p1Points, p2Points
+
     #init objects
     PLAYER1 = Paddle((20, 100), (60, HEIGHT // 2 - 50))
     PLAYER2 = Paddle((20, 100), (WIDTH - 70, HEIGHT // 2 - 50))
     BALL = Ball((20, 20), (WIDTH//2 - 10, HEIGHT//2 - 10))
 
-    BALL.xVel = -1# default vel
+    BALL.xVel = -3# default vel
 
     run = True
+    restart = True
     clock = pygame.time.Clock()#defines the clock
 
     while run:# game loop
@@ -86,10 +113,12 @@ def main():
         for event in pygame.event.get():#loops through the events
             if event.type == pygame.QUIT:#if it is quit, quit
                 run = False
+                restart = False
 
             if event.type == pygame.KEYDOWN:# runs when a key is pressed
                 if event.key == pygame.K_ESCAPE:# if escape is pressed, escape
                     run = False
+                    restart = False
         
         keys_pressed = pygame.key.get_pressed()# gets all the keys
         
@@ -102,6 +131,17 @@ def main():
         if keys_pressed[pygame.K_DOWN] and PLAYER2.y + VEL + PLAYER2.HEIGHT < HEIGHT:# moves player2 down if in bounds
             PLAYER2.y += VEL
 
+        if BALL.x < 0: # if the ball is on the left increace the score by 1 and restart
+            p2Points += 1
+            run = False
+        elif BALL.x + BALL.WIDTH > WIDTH:# if the ball is on the right increace the score by 1 and restart
+            p1Points += 1
+            run = False
+
+        #renders the fonts
+        p1Score_text = SCORE_FONT.render(str(p1Points), 1, WHITE)
+        p2Score_text = SCORE_FONT.render(str(p2Points), 1, WHITE)
+
         WIN.fill(BLACK)# fills the screen
 
         BALL.move((PLAYER1, PLAYER2)) #move the ball
@@ -111,9 +151,20 @@ def main():
         PLAYER2.make_it()
         BALL.make_it()
 
+        #makes the score
+        WIN.blit(p1Score_text, ((WIDTH//2 - 50) - 100, 0))
+        WIN.blit(p2Score_text, ((WIDTH//2 - 50) + 100, 0))
+
         pygame.display.update()# updates the display
         
-    pygame.quit()#quits
+        if checkWin():# checkes if there is a winner
+            run = False
+            restart = False
+    
+    if restart:# if it is being restarted, restart
+        main()
+    else:#otherwise, quit
+        pygame.quit()
 
 # if it is being imported run the main
 if __name__ == '__main__':
